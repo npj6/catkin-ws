0033e2b45915bc16427c4839535b1ce573a0d46b
Pomares <npj6@alu.ua.es>
2021-03-24 16:41:55 +0100

    suscripcion a topicos, pendiente nuevas clases informacion y metodo virtual comportamiento

--- src/var_ctrl/src/controller.cpp
+++ src/var_ctrl/src/controller.cpp
@@ -1,22 +1,41 @@
 #include "controller.h"
 
+/*DepthInfo, incluir por composicion, modulos de informacion a añadir, controller solo controla*/
+
 namespace VAR_CTRL {
   Controller::Controller(ros::NodeHandle& nh, std::string robot_name) {
     forward_speed=1.0;
     rotation_speed=0.0;
     // segundo parametro: si acumulamos varios mensajes, solo el último será enviado.
     movement = nh.advertise<geometry_msgs::Twist>("/"+robot_name+"/mobile_base/commands/velocity", 1);
+    depth_pc2[front] = nh.subscribe(
+      "/"+robot_name+"/camera/depth/points", 1, &Controller::depth_pc2_callback<front>, this
+    );
+    depth_pc2[back1] = nh.subscribe(
+      "/"+robot_name+"/trasera1/trasera1/depth/points", 1, &Controller::depth_pc2_callback<back1>, this
+    );
+    depth_pc2[back2] = nh.subscribe(
+      "/"+robot_name+"/trasera2/trasera2/depth/points", 1, &Controller::depth_pc2_callback<back2>, this
+    );
   }
 
   void Controller::loop(void) {
     ros::Rate rate(10); // frecuencia del bucle en hertzios (normalmente 100)
     while (ros::ok()) {
-      geometry_msgs::Twist msg;
-      msg.linear.x = forward_speed;
-      msg.angular.z = rotation_speed;
-      movement.publish(msg);		
+      move();
       ros::spinOnce(); // procesar llamadas pendientes (callBacks de suscripciones)
       rate.sleep();
     }
+  }
+
+  void Controller::move(void) {
+    geometry_msgs::Twist msg;
+    msg.linear.x = forward_speed;
+    msg.angular.z = rotation_speed;
+    movement.publish(msg);
+  }
+
+  void Controller::depth_pc2_callback(const sensor_msgs::PointCloud2::ConstPtr& msg, Cam camera) {
+    std::cout << "CALLBACK from " << camera << std::endl;
   }
 }
--- src/var_ctrl/src/controller.h
+++ src/var_ctrl/src/controller.h
@@ -2,26 +2,32 @@
 
 #include "ros/ros.h"
 #include "geometry_msgs/Twist.h"
+#include "sensor_msgs/PointCloud2.h"
+#include "sensor_msgs/PointField.h"
 #include <string>
 
+#include <iostream>
 
-/*
-      ejemplo suscripciones
-      ros::Subscriber trasera1;
-      ros::Subscriber trasera2;
-      ros::Subscriber delantera;
-      laserSub = nh.subscribe("base_scan", 1, &Wander::commandCallback, this);
-      void commandCallback(const sensor_msgs::LaserScan::ConstPtr& msg)
-*/
 
 namespace VAR_CTRL {
+  enum Cam {front, back1, back2, TOTAL_NUM};
+
   class Controller {
     protected:
       ros::Publisher movement;
+      ros::Subscriber depth_pc2[TOTAL_NUM];
 
       double forward_speed;
       double rotation_speed;
 
+    void move(void);
+
+    template<Cam camera>
+    void depth_pc2_callback(const sensor_msgs::PointCloud2::ConstPtr& msg) {
+    	depth_pc2_callback(msg, camera);
+    }
+
+    void depth_pc2_callback(const sensor_msgs::PointCloud2::ConstPtr& msg, Cam camera);
     public:
       Controller(ros::NodeHandle& nh, std::string robot_name);
       void loop(void);
